dfe83cad12a78f41a9035630a1e5b94b416e4b58
Ax Red <axred1967@gmail.com>
2017-03-28 12:17:26 +0200

    ccc

--- js/add_document_ang.js
+++ js/add_document_ang.js
@@ -203,9 +203,9 @@ app2.controller('personCtrl', function ($scope,$http,$translate) {
         data={ "action":"get_document_image_name_multi", id:id}
         $http.post( SERVICEURL2,  data )
             .success(function(data) {
-              alert(data)
                       if(data.RESPONSECODE=='1') 			{
-                        $scope.Doc.doc_image=data.RESPONSE.imagename;
+                        $http.post( LOG,  {data:data.RESPONSE})
+                        $scope.Doc.doc_image=data.RESPONSE;
                       }
              })
             .error(function() {
