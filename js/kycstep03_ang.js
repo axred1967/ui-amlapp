25a480f46e2bca3b0c5fa630ee1bba259865b4e3
Ax Red <axred1967@gmail.com>
2017-04-18 05:10:13 +0200

    step 3

--- js/kycstep03_ang.js
+++ js/kycstep03_ang.js
@@ -1,3 +1,33 @@
+var app = {
+    initialize: function(){
+        this.bind();
+    },
+    bind: function() {
+        document.addEventListener('deviceready', getChkLogin, false);
+    },
+    deviceready: function() {
+        // This is an event handler function, which means the scope is the event.
+        // So, we must explicitly called `app.report()` instead of `this.report()`.
+        app.report('deviceready');
+    },
+    report: function(id) {
+        // Report the event in the console
+        console.log("Report: " + id);
+    },
+};
+
+function getChkLogin()
+{
+
+        chkloggedin();
+
+}
+
+setTimeout(function(){
+        checkthesidebarinfouser();
+}, 800);
+
+
 var app2 = angular.module('myApp', ['pascalprecht.translate','ng-currency','fieldMatch']);
 //Field Match directive
 angular.module('fieldMatch', [])
